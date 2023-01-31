c5fd4ae20138e8bf8d66b5d0086ba4cd2b266c7d
Ade Maulana Akbar <maulanaakbarr90@gmail.com>
2023-01-31 10:47:59 +0700

    add validation login

--- src/Pages/loginPages.js
+++ src/Pages/loginPages.js
@@ -3,8 +3,35 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
+import { useForm } from "react-hook-form"
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 const LoginPages = () => {
+
+	const { register, handleSubmit , formState: { errors } } = useForm();
+
+	const url = `http://10.202.100.84:3003/api/getdata_staff`
+    const getDB = async () => {
+    const datas = await axios.get(url)
+    return datas
+    }
+
+	const [ dbUser, setDbUser] = useState([])
+	
+	useEffect (() => {
+    getDB().then((result) => {
+    setDbUser (result.data.data)
+    console.log("result :", dbUser)
+    })   
+     },[])
+
+	const onSubmit = datas => {
+	console.log(datas)
+	alert(JSON.stringify(datas))
+	}
+
   return(
 		 <div class="ftco-section">
 		  <div class="container">
@@ -15,16 +42,17 @@ const LoginPages = () => {
                 			<img class="img-fluid max-width: 100%" alt="logo dept. NIN" src={login1}></img>
                 				</div>
 
-							  <div class="login-wrap p-4 p-md-4">
+							  		<div class="login-wrap p-4 p-md-4">
 			      					<div class="w-100 ">
 			      						<h1 class="mb-2 text-dark font-weight-bold ">Login</h1>
 			      					</div>
 
-									<Form>
+									<Form onSubmit={handleSubmit(onSubmit)}>
       								<Row className="mb-2">
         							<Form.Group as={Col} controlId="formGridCity">
-          							<Form.Label>Id User</Form.Label>
-          							<Form.Control />
+          							<Form.Label>ID USER</Form.Label>
+          							<Form.Control {...register("staff_name", { required: true })} />
+									{errors?.staff_name?.type === "required" && <p>Masukan ID USER</p>}
 
         							</Form.Group>
 									<Form.Group as={Col} controlId="formGridCity">
@@ -40,28 +68,14 @@ const LoginPages = () => {
 									</Row>
 									<Row className="mb-3">
         							<Form.Group as={Col} controlId="formGridCity">
-          							<Form.Label>Password</Form.Label>
-          							<Form.Control />
-        							</Form.Group>
+          							<Form.Label>PASSWORD</Form.Label>
+          							<Form.Control type='password' {...register("password", { required: true })} />
+        							{errors?.password?.type === "required" && <p>Masukan PASSWORD</p>}
+									</Form.Group>
 									</Row>
 									<Button variant="primary" type="submit">Login </Button>
 									</Form>
 									
-										{/* <form action="#" class="signin-form ">
-			      							<div class="form-group mt-3">
-                          <label class="form-control-placeholder" for="username">Username
-			      								<input type="text" class="form-control" value=""></input>
-                            </label>
-			      							</div>
-		            						<div class="form-group mt-3">
-                            <label class="form-control-placeholder" for="password">Password
-		              							<input id="password-field" type="password" class="form-control" name=""></input>
-		             							</label>
-		            						</div>
-		            						  <div class="form-group mt-3">
-		            							<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
-		           							 </div>
-		          						</form> */}
 		        			</div>
 		      			</div>
 					</div>
